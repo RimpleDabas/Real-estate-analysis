7636a043fc15dac989bc50760c76c068ac543b48
Rimple Poonia Dabas <125090606+RimpleDabas@users.noreply.github.com>
2024-04-30 20:56:38 -0400

    Update Analysis.sql

--- Analysis.sql
+++ Analysis.sql
@@ -86,7 +86,7 @@ WHERE arp.avg_rent IN (SELECT max_avg_rent FROM MaxAvgRent)
    OR arp.avg_rent IN (SELECT min_avg_rent FROM MinAvgRent);
 
 
-
+-- We can create view to get insights for the Provinces based on the years
 CREATE VIEW RentalSummary AS
 SELECT r.Rent_ID, r._1_Bedroom, r.province_id, r.neighbourhood_id, r.YEAR, p.Provinces
 FROM [Rental Data] r
@@ -104,13 +104,33 @@ GROUP BY  p.Provinces,c.Centres,r.Year);
 
 
 SELECT * FROM Provincial_data; 
-
+-- Getting data for the provinces for all years top 5
 SELECT TOP 5 Provinces,Year , ROUND(AVG([1_BHK]),2) AS rents FROM Provincial_data
 GROUP BY Provinces,Year
 ORDER BY YEAR DESC, ROUND(AVG([1_BHK]),2) DESC;
-	
+
+
+-- filter based on the year top 5
 SELECT TOP 5 Provinces ,ROUND(AVG([1_BHK]),2) AS rents FROM Provincial_data
-WHERE YEAR = '2022'
+WHERE YEAR = '2018'
 GROUP BY Provinces
+ORDER BY ROUND(AVG([1_BHK]),2) DESC;
+	
+
+CREATE VIEW Centres_data AS 
+	(SELECT p.Provinces, c.centres, r.YEAR, ROUND(AVG(r._1_Bedroom),2) AS '1_BHK' 
+		FROM [Rental Data] r
+		INNER JOIN Provinces p ON r.province_id = p.province_id
+		RIGHT JOIN Centres c  ON r.Centre_id = c.Centre_id
+		WHERE r._1_Bedroom > 0
+		GROUP BY  p.Provinces,c.Centres,r.Year)
+
+SELECT * FROM Centres_data;
+
+-- WE can use this to select data based on the Provinces and Year 
+
+SELECT TOP 3 Centres ,ROUND(AVG([1_BHK]),2) AS rents FROM Centres_data
+WHERE YEAR = '2018' AND Provinces = 'B.C.'
+GROUP BY Centres
 ORDER BY ROUND(AVG([1_BHK]),2) DESC;
 	
